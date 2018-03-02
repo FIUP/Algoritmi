c16e1224f8511b46677241ae13ae213cd6289dd5
Mou95 <maurocarlin3@gmail.com>
2018-03-02 20:49:42 +0100

    Creato grafico primo es, manca normalizzazione

--- lab1/ex1.py
+++ lab1/ex1.py
@@ -1,4 +1,7 @@
-from utils.inDegree import inDegree 
+from utils.inDegree import inDegree
+import numpy
+import matplotlib.pyplot as pyp
+import tkinter
 
 def buildGraphAdjacencyMatrix(file_name):
     f = open(file_name)
@@ -6,27 +9,43 @@ def buildGraphAdjacencyMatrix(file_name):
     node_list = []
     for line in f:
         for word in line.split():
-           if i == 0:
-               from_node_id = word
-               i = i + 1
-           else:
-               to_node_id = word
-               j = 0 
-               if(isInTheList(to_node_id,node_list)):
-                   node_list[j].inDegree = node_list[j].inDegree + 1
-               else:
-                    
-               i = 0
+            if i == 0:
+                from_node_id = word
+                i = i + 1
+            else:
+                to_node_id = word
+                print(to_node_id)
+                j = 0
+                trovato = False
+
+                while (j < len(node_list) and not trovato):
+                    if(to_node_id == node_list[j].id_node):
+                        node_list[j].in_degree = node_list[j].in_degree + 1
+                        trovato = True
+                    j = j + 1
+
+                if (not trovato):
+                    x = inDegree(to_node_id)
+                    node_list.append(x);
+
+                i = 0
+    return node_list
+
+def buildGraphInDegreeDistribution(node_list):
+    distribution = numpy.zeros(27770)
+    for node in node_list:
+        distribution[node.in_degree] = distribution[node.in_degree] + 1
+
+    return distribution
+
+def distributionInDegree(in_degree, totNodes):
+    for grade in in_degree:
+        grade = grade / totNodes
 
 
-def isInTheList(id,node_list):
-    j = 0
-    while j < len(node_list):
-        if(id == node_list[j].id_node)
-            return True
-    return False 
 
-
-    
-buildGraphAdjacencyMatrix("Cit-HepTh.txt")
-
+node_list = buildGraphAdjacencyMatrix("Cit-HepTh.txt")
+in_degree = buildGraphInDegreeDistribution(node_list)
+in_degree = list(map(lambda x: x/27770, in_degree))
+pyp.loglog(in_degree)
+pyp.show()
--- lab1/utils/inDegree.py
+++ lab1/utils/inDegree.py
@@ -1,6 +1,4 @@
 class inDegree:
-    
     def __init__(self,id):
         self.id_node = id
         self.in_degree = 1
-    
