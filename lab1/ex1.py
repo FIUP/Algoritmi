c5f423f553d220f7ee3480e9d486bf3e4e6a021d
BottCode <mattiabottaro9@gmail.com>
2018-03-06 10:17:35 +0100

    fixato count nodi indegree=0

--- lab1/ex1.py
+++ lab1/ex1.py
@@ -1,10 +1,3 @@
-''' Questa domanda richiede di analizzare il grafo delle citazioni di 27770 articoli di fisica delle alte energie.
-Il grafo ha 352807 archi ed è contenuto nel file allegato. Ogni articolo del dataset è identificato da un numero.
-Ogni riga del file contiene un arco del grafo dove il primo numero è la coda ed il secondo la testa dell'arco.
-
-Il compito da svolgere per questa domanda è di calcolare la distribuzione del grado entrante del grafo delle citazioni.
-La distribuzione deve essere normalizzata (i valori devono sommare a 1).
-Il risultato deve essere mostrato in un grafico di dispersione (o plot) dei punti con doppia scala logaritmica (log-log plot). '''
 
 import utils.buildGraphInDegreeDistribution as bG
 from utils.inDegree import inDegree
@@ -19,10 +12,23 @@ def buildGraphAdjacencyMatrix(file_name):
         for word in line.split():
             if i == 0:
                 from_node_id = word
+                
+                k = 0
+                trovato = False
+
+                while (k < len(node_list) and not trovato):
+                    if(from_node_id == node_list[k].id_node):
+                        trovato = True
+                    k = k + 1
+
+                if (not trovato):
+                    x = inDegree(from_node_id,0)
+                    node_list.append(x);
+
                 i = i + 1
             else:
                 to_node_id = word
-                print(to_node_id)
+                #print(to_node_id)
                 j = 0
                 trovato = False
 
