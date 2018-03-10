a031b569b4fcc1997974660f3a2fd9344cb57cc4
Mou95 <maurocarlin3@gmail.com>
2018-03-10 10:46:28 +0100

    Sistemato ex3

--- lab1/ex3.py
+++ lab1/ex3.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 import numpy as np
 import matplotlib.pyplot as pyp
 import random
@@ -9,21 +6,17 @@ class DPATrial:
     def __init__(self,num_nodes):
         self.num_nodes = num_nodes #numero di nodi del grafo
         self.node_numbers = [] #lista di numeri di nodo da cui estrarre
-        self.E = set()
         for i in range(m):
             for j in range (m):
                 self.node_numbers.append(i)
-                if (i != j):
-                    self.E.add((i,j))
 
 
     def runTrial(self, m):
 
-        V = []
+        V = set()
         for i in range(m): #pesco m numeri a caso e li inserisco nella lista V (aumenterà il loro indegree)
             u = random.choice(self.node_numbers)
-            V.append(u)
-            self.E.add((self.num_nodes, u))
+            V.add(u)
 
         self.node_numbers.append(self.num_nodes) #aggiungo il nuovo nodo
         self.node_numbers.extend(V) #aumento l'indegree dei nodi pescati
@@ -41,19 +34,14 @@ for k in range(n - p.num_nodes): #pesco fino ad avere 27770 nodi
 print (p.num_nodes)
 print (len(p.node_numbers))
 
-V = set()
-
-#creo V
-for node in p.node_numbers:
-    V.add(node)
-
 #Calcolo l'in-degree di ogni nodo
 in_degree = dict()
-for node in V:
-    in_degree[node] = 0
+for node in p.node_numbers:
+    in_degree[node] = -1
 
-for (_, n) in p.E:
+for n in p.node_numbers:
     in_degree[n] += 1
+
 
 #Calcolo la distribuzione dell'in-degree
 distribution = np.zeros(27770)
