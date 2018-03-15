dfaab699f5a7129eea02cb9904cdceda871a151f
BottCode <mattiabottaro9@gmail.com>
2018-03-15 23:58:48 +0100

    Aggiunta classe che genera un grafo non orientato dal file txt

---
+++ lab2/utils/generateGraphFromFile.py
@@ -0,0 +1,33 @@
+# questo script genera un grafo NON orientato dal file ti testo
+import itertools
+from collections import defaultdict
+import numpy as np
+
+class generateGraphFromFile:
+    def __init__(self,path):
+        self.path = path
+        self.E = set()
+        self.V = set()
+        # lista delle adj: e un dizionario il quale associa un set ad ogni chiave. Il set contiene i nodi adiacenti
+        self.adj_list = defaultdict(set)
+        f = np.loadtxt(self.path, delimiter="\t")
+        for line in f:
+            # la lettura dal file mi ritorna sempre float, quindi li converto in int
+            # brutto ...
+            l = [int(n) for n in line] 
+            # print l
+            # aggiungo i nodi a V
+            self.V.add(l[0])
+            self.V.add(l[1])
+            # Aggiungo l'arco a E, tenendo conto che il grafo NON orientato
+            # itertools.combination: https://stackoverflow.com/questions/35047737/unordered-pairs-pair-sets-in-python-from-a-list
+            for edge in itertools.combinations(l,2):
+                # senza sorted(edge) otterremmo una lista che distingue le coppie non ordinate
+                self.E.add(tuple(sorted(edge)))
+            self.adj_list[l[0]].add(l[1])
+        print self.V
+        print self.E
+        print self.adj_list
+
+# generateGraphFromFile("../as19991212.txt")
+
