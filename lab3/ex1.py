4882f6a0ebf955ab7f4b79349877e849a47b6021
pierz95 <tricomipierpaolo@gmail.com>
2018-03-27 10:17:06 +0200

    mezzo dijsktra

--- lab3/ex1.py
+++ lab3/ex1.py
@@ -1,6 +1,8 @@
 import heapq
+import weightedGraphFromFile.py as weightedGraph
+import math
 
-''' Usate l'algoritmo CCRP per trovare un piano di evacuazione nel grafo della Città di San Francisco. 
+''' Usate l'algoritmo CCRP per trovare un piano di evacuazione nel grafo della Città di San Francisco.
 I nodi sorgente del piano sono i tre ingressi autostradali della città:
 
 nodo 3718987342 (Golden Gate Bridge)
@@ -14,7 +16,36 @@ nodo 65325408 (Saint Mary's Medical Center)
 nodo 65295403 (CPMP California Campus)
 nodo 258913493 (Kaiser-Permanente Medical Center)
 
-Mostrate l'andamento dell'algoritmo in un grafico a linee dove l'asse x corrisponde alle capacità 
-e l'asse y al tempo di percorrenza. Il grafico deve mostrare come cresce la capacità ed il tempo di percorrenza 
+Mostrate l'andamento dell'algoritmo in un grafico a linee dove l'asse x corrisponde alle capacità
+e l'asse y al tempo di percorrenza. Il grafico deve mostrare come cresce la capacità ed il tempo di percorrenza
 al crescere del numero di percorsi inseriti nel piano dall'algoritmo. '''
 
+def initSSSP(d,pi,s, V):
+    for node in V:
+        d[node] = math.inf #cast a float forse
+        pi[node] = None
+    d[s] = 0
+
+def Relax(road_time, d, pi, u, v):
+    d[v] = d[u] + road_time
+    pi[v] = u
+
+def Dijkstra(graph, s):
+    l1 = graph.weigthed_adj_list.keys()
+
+    V = set(l1)
+
+    d = dict()
+    pi = dict()
+
+    initSSSP(d,pi,s, V)
+    Q = list(V)
+    heap = list()
+    for node in V:
+        heapq.heappush(heap,graph.getRoadTime(s,node) (V)) #boh
+    while Q:
+        u = heapq.heappop(Q) #min
+        for node in graph.weigthed_adj_list[u].keys():
+            if d[u] + RT < d[node]:
+                Relax (RT,d,pi,u,v)
+                Decrease
