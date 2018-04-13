cbaa33c6d576d98834aa220f97d74f2be9e97398
Mou95 <mauro.carlin3@gmail.com>
2018-04-13 11:48:40 +0200

    Creato dizionario delle combinazioni nodo+insieme di nodi

--- lab4/utils/dictGenerator.py
+++ lab4/utils/dictGenerator.py
@@ -1,24 +1,25 @@
-import numpy as np
-from collections import defaultdict
 import itertools
-import random
-import sys
 
-def dictGenerator():
-    n = 200#len(matrix)
-    dictionary = dict()
+def dictGenerator(n):
+    dictionary = {}
     d = []
     pi = []
+    #prendo 1 come nodo iniziale, con unica combinazione: tutti i nodi
     dictionary[1] = [x+1 for x in range(n)]
     d.append(None)  #valore della soluzione
     pi.append(None)
+    #lista da dove generare le combinazioni
+    l = [x for x in range (2,n+1)]
+    #creo tutte le combinazioni
+    combination = []
+    for r in range (len(l)+1):
+        combination.extend(map(list,itertools.combinations(l,r)))
+    #filtro le combinazioni che hanno i al loro interno
     for i in range(2,n+1):
-        l = [x for x in range (1,n+1) if x!=1]
         dictionary[i] = []
-        for r in range (len(l)+1):
-            comb = map(list,itertools.combinations(l,r)) #migliorabile trovando il modo di generare le sole combinazioni con dentro i
-            comb = [x for x in comb if i in x]
-            dictionary[i].extend(comb)
-    #print(dictionary)
+        comb = [x for x in combination if i in x]
+        dictionary[i].extend(comb)
+    print(dictionary)
+    return dictionary
 
-dictGenerator()
+#dictGenerator()
